3a2cea5a8226929f98cdf01c113ef96e95f0297b
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-02-18 17:39:02 +0100

    updating git

--- script/prepare_linux_arm.sh
+++ script/prepare_linux_arm.sh
@@ -6,5 +6,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install binutils build-essential -y
 apt-get install python3 -y
-apt-get install git fontconfig libfontconfig1-dev libgl-dev -y
-apt-get install clang -y
+apt-get install fontconfig libfontconfig1-dev libgl-dev -y
+apt-get install software-properties-common -y
+add-apt-repository ppa:git-core/ppa -y
+apt-get update -y
+apt-get install git clang -y
